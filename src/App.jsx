cb591a87533e22e2f8c4cede0671e13c902d3498
Feroz <mohamedferoz2023@gmail.com>
2025-10-21 11:13:06 +0530

    fix background color

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     <>
       <div className="overflow-hidden text-neutral-300 antialiased select-none selection:bg-cyan-300 selection:text-black">
         <div className="fixed top-0 -z-10 min-h-screen w-full">
-          <div className="absolute inset-0 -z-10 min-h-screen w-screen -ml-10 [background:radial-gradient(130%_120%_at_50%_20%,#000_55%,#800080_100%)]"></div>
+          <div className="absolute inset-0 -z-10 min-h-screen w-screen -ml-10 [background:radial-gradient(130%_120%_at_50%_20%,#001_55%,#7F00FF_100%)]"></div>
         </div>
         <Navbar />
         <HeroSection />
